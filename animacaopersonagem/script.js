b0256b45d3425766ff38a2f0fe5e06a2fa5c3c14
Kleber Amaro <kleberamarosilva@gmail.com>
2026-06-29 11:46:32 -0300

    atualização personagem

---
+++ animacaopersonagem/script.js
@@ -0,0 +1,22 @@
+const dai=document.querySelector("#dai");
+const caract=document.querySelector("ul");
+
+dai.addEventListener("mouseover",()=>{
+    let texto = "Item1";
+    for(let i=0;i<texto,length; i++){
+
+        setTimeout(()=>{
+            let valor=
+            caract.innerHTML="<li>Item 1</li>"
+
+        },0.5)
+    }
+
+    setInterval(()=>{
+        caract.innerHTML="<li>Item 1</li>"
+    })
+    
+
+    
+
+})
